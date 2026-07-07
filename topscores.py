38f9043657240b536f5637ebad75c49c0cf30e1f
herCoconut <hergyalog@live.com>
2026-07-07 13:50:23 +0000

    Maximo goleadores

--- topscores.py
+++ topscores.py
@@ -9,7 +9,12 @@ def main():
         topscorers = get_topscorers(season=season)
         if topscorers:
             print(f"Temporada {season}:")
-            print(topscorers[:5])
+            # topscorers es una lista de diccionarios, por eso no se puede hacer topscorers["player"]
+            for scorer in topscorers:
+                player_name = scorer["player"]["name"]
+                team_name = scorer["statistics"][0]["team"]["name"]
+                goals = scorer["statistics"][0]["goals"]["total"]
+                print(f"{player_name} - {team_name} - Goles: {goals}")
             break
     else:
         print("No se encontraron datos de máximos goleadores.")
