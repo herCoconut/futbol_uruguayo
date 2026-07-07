36ccbfc4a4a295636badff03802908abff097aa0
herCoconut <hergyalog@live.com>
2026-07-07 14:34:44 +0000

    Se agrega minutos por gol, posicion y partidos jugados.

--- topscores.py
+++ topscores.py
@@ -5,16 +5,21 @@ from api import get_topscorers
 
 
 def main():
-    for season in (2026, 2025, 2024, 2023, 2022, 2021, 2020):
+    for season in (2026, 2025, 2024, 2023, 2022, 2021, 2020):   # Temporadas habilitadas: 2024, 2023 y 2022
         topscorers = get_topscorers(season=season)
         if topscorers:
+            # print(topscorers[0])
             print(f"Temporada {season}:")
             # topscorers es una lista de diccionarios, por eso no se puede hacer topscorers["player"]
             for scorer in topscorers:
                 player_name = scorer["player"]["name"]
                 team_name = scorer["statistics"][0]["team"]["name"]
                 goals = scorer["statistics"][0]["goals"]["total"]
-                print(f"{player_name} - {team_name} - Goles: {goals}")
+                position = scorer["statistics"][0]["games"]["position"]
+                appearances = scorer["statistics"][0]["games"]["appearences"]
+                minutes_played = scorer["statistics"][0]["games"]["minutes"]
+                minutes_per_goal = minutes_played / goals if goals > 0 else None
+                print(f"{player_name} - {team_name} - Goles: {goals} - Posición: {position} - Apariciones: {appearances} - Minutos por gol: {int(minutes_per_goal)}")
             print("\n")
     else:
         print("No se encontraron datos de máximos goleadores.")
