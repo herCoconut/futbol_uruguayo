2c009ac3108d8ff1b8793dc21711c98da1d88d54
herCoconut <hergyalog@live.com>
2026-07-08 15:12:02 +0000

    Se le dio la estrictura base a standings.py.

---
+++ standings.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from api import get_team_stats
+
+
+def main():
+    # Introducir las funcion que se crean
+    temporadas_habilitadas()
+
+# Crear las funciones que se utilizaran en el codigo
+
+def temporadas_habilitadas():
+    
+
+if __name__ == "__main__":
+    main()
