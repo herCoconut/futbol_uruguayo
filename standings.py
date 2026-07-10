6a8c081f462bd07b1d0877f772da6914d15f044e
herCoconut <hergyalog@live.com>
2026-07-10 15:05:26 +0000

    Se creo el dict colores de los equipos para poder graficar los equipos cada uno con sus respectivos colores. (Falta terminar)

--- standings.py
+++ standings.py
@@ -27,6 +27,25 @@ def ranking(season=2024, league=None, cache_ttl=3600, force=False):
     print(f"\033[103mColumnas del DataFrame: \033[0m{df.columns.tolist()}")
     print(f"\033[103mSe muestra los nombres de los equipos: \033[0m{df['team_name'].tolist()}")
 
+    colores_equipos = {
+        "Penarol": "#FFC040",
+        "Nacional": "#FFFFFF",
+        "Defensor sporting": "#A000A0",
+        "Boston River": "#11E91C",
+        "Progreso": "#FF0000",
+        "Cerro Largo": "#0000AA",
+        "Racing Montevideo": "#00AA00",
+        "Liverpool Montevideo": "#0000FF",
+        "Wanderers": "#111111",
+        "Cerro": "#44BBFF",
+        "Deportivo Maldonado": "#",
+        "Rampla Juniors": "#",
+        "CA River Plate": "#",
+        "Danubio": "#",
+        "Fenix": "#",
+        "Miramar": "#",
+    }
+
 
 
 
